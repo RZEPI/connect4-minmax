b74a880ad9585daa8cd2cdb284e8804c49d41489
Paweł Rzepecki <pawel.rzepecki21@gmail.com>
2023-04-04 10:08:47 +0200

    alpha beta implementation

--- alphabetaagent.py
+++ alphabetaagent.py
@@ -1,10 +1,15 @@
 import minmaxagent as MinMaxAgent
 import math
 class AlphaBetaAgent(MinMaxAgent.MinMaxAgent):
-     def __init__(self, my_token):
-         super().__init__(my_token)
+    def __init__(self, my_token):
+        super().__init__(my_token)
 
-     def minimax(self, connect4, depth, maximazing_player):
+
+    def decide(self, connect4):
+        move, _ = self.minimax(connect4, 4, True, -math.inf, math.inf)
+        return move
+
+    def minimax(self, connect4, depth, maximazing_player, alpha, beta):
         if self.my_token == 'x':
             opponent = 'o'
         else:
@@ -26,19 +31,25 @@ class AlphaBetaAgent(MinMaxAgent.MinMaxAgent):
             value = -math.inf
             for col in connect4.possible_drops():
                 connect4.drop_token_minmax(col)
-                _, tmp_value = self.minimax(connect4, depth-1, False)
+                _, tmp_value = self.minimax(connect4, depth-1, False, alpha, beta)
                 connect4.pull_token(col)
                 if value < tmp_value:
                     best_col = col
                     value = tmp_value
+                alpha = max(value, alpha)
+                if alpha >= beta:
+                    break
         else:
             value = math.inf
             for col in connect4.possible_drops():
                 connect4.drop_token_minmax(col)
-                _, tmp_value =self.minimax(connect4, depth-1, True)
+                _, tmp_value =self.minimax(connect4, depth-1, True, alpha, beta)
                 connect4.pull_token(col)
                 if value > tmp_value:
                     best_col = col
                     value = tmp_value
+                beta = min(beta, value)
+                if alpha >= beta:
+                    break
 
         return best_col, value
